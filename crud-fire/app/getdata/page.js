39ef9ec2553478aaffc21bf0856acdabc164360c
Mashoodgondal <mashoodgondal6146@gmail.com>
2024-09-30 18:22:22 +0500

    delete-doc

--- crud-fire/app/getdata/page.js
+++ crud-fire/app/getdata/page.js
@@ -1,6 +1,13 @@
 "use client";
 import { db } from "@/config/firebase";
-import { collection, getDocs } from "firebase/firestore";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 
 const GetData = () => {
@@ -8,10 +15,11 @@ const GetData = () => {
   const fetchData = async () => {
     try {
       const collectionName = collection(db, "abs");
+      // const Q = query(collectionName, where("userName", "==", "ahmad"));
       const docs = await getDocs(collectionName);
       const studentData = [];
       docs.forEach((doc) => {
-        studentData.push(doc.data());
+        studentData.push({ id: doc.id, ...doc.data() });
       });
       setstudents(studentData);
       console.log("students", studentData);
@@ -22,12 +30,25 @@ const GetData = () => {
   useEffect(() => {
     fetchData();
   }, []);
+
+  const deleteHandeler = async (id) => {
+    const docRef = doc(db, "abs", id);
+    await deleteDoc(docRef);
+    // fetchData();
+    // another way
+    const newStudent = students.filter((student) => id == !student.id);
+    setstudents(newStudent);
+  };
+
   return (
     <div className="container mx-auto my-8">
       <h1 className="text-2xl font-bold mb-6 text-center">List of Students</h1>
       <table className="min-w-full bg-white border border-gray-300 shadow-md">
         <thead>
           <tr className="bg-gray-200 text-gray-700">
+            <th className="py-3 px-4 border-b-2 border-gray-300 text-left">
+              ID
+            </th>
             <th className="py-3 px-4 border-b-2 border-gray-300 text-left">
               Name
             </th>
@@ -37,11 +58,20 @@ const GetData = () => {
             <th className="py-3 px-4 border-b-2 border-gray-300 text-left">
               Courses
             </th>
+            <th className="py-3 px-4 border-b-2 border-gray-300 text-left">
+              Delete
+            </th>
+            <th className="py-3 px-4 border-b-2 border-gray-300 text-left">
+              Update
+            </th>
           </tr>
         </thead>
         <tbody>
           {students?.map((student, index) => (
             <tr key={index} className="hover:bg-gray-100">
+              <td className="py-2 px-4 border-b border-gray-300">
+                {student.id}
+              </td>
               <td className="py-2 px-4 border-b border-gray-300">
                 {student.userName}
               </td>
@@ -50,6 +80,14 @@ const GetData = () => {
               </td>
               <td className="py-2 px-4 border-b border-gray-300">
                 {student.course}
+              </td>
+              <td>
+                <button onClick={() => deleteHandeler(student.id)}>
+                  delete
+                </button>
+              </td>
+              <td>
+                <button>update</button>
               </td>
             </tr>
           ))}
